00f35de275985e74442792bcdf723d50ca067370
rtexelm <texelist@gmail.com>
2022-10-12 16:29:07 -0400

    Added quesitons class

--- questiondb.rb
+++ questiondb.rb
@@ -3,4 +3,60 @@ require 'singelton'
 
 class QuestionsDB < SQLite3::Database
     include Singleton
-    
+
+    def initialize
+        super('plays.db')
+        self.type_translation = true
+        self.results_as_hash = true
+    end
+end
+
+class User
+
+    def self.find_by_id(id)
+        user = QuestionsDB.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?
+        SQL
+        return nil unless user.length > 0
+
+        User.new(user) 
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end
+
+end
+
+class Question
+
+    def self.find_by_id(id)
+        question = QuestionsDB.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                questions
+            WHERE
+                id = ?
+        SQL
+        return nil unless question.length > 0
+
+        Question.new(question) 
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end
+
+end
